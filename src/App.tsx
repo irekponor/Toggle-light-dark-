3fe0b60386126674566668b0106c87c108b3096e
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-16 04:03:33 +0100

    hine

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <h1 className="title">Hii Peeps</h1>
+    </div>
+  );
 };
 
 export default App;
