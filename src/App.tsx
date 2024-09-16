936623ff5dda9533dca3f05e45ace62b0c8c29eb
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-16 04:09:46 +0100

    first

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="title">Hii Peeps</h1>
+      <div className="box">
+        <h2>This is a box</h2>
+      </div>
     </div>
   );
 };
