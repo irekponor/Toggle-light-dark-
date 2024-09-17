4ba04098eae4707ad80ced9ff8702d22265333ee
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-17 01:10:45 +0100

    navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,6 +3,10 @@ const Navbar = () => {
     <div>
       <nav className="">
         <h3 className="logo-text">Country Facts</h3>
+
+        <div className="toggler--slider">
+          <div className="toggler--slider--circle"></div>
+        </div>
       </nav>
     </div>
   );
