eef4cf50f8fd718c6043bba3325dd4394dbd906d
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-17 01:14:28 +0100

    main'

---
+++ src/Content.tsx
@@ -0,0 +1,15 @@
+const Content = () => {
+  return (
+    <div>
+      <h1 className="main-title">Fun facts about some countries</h1>
+      <ul className="main-facts">
+        <li>
+          Australia is famous for Beaches, kangaroos, Great Barrier Reef, Sydney
+          Opera House etecera.
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Content;
