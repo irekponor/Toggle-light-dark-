d774d11065415b2ce7406e44f5aef4f5fd789c39
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-17 01:16:37 +0100

    main'

--- src/Content.tsx
+++ src/Content.tsx
@@ -7,6 +7,11 @@ const Content = () => {
           Australia is famous for Beaches, kangaroos, Great Barrier Reef, Sydney
           Opera House etecera.
         </li>
+        <li>
+          Mexico is famous for ancient Mayan and Aztec ruins Tulum, Chichen
+          Itza, beaches, Cancun, Playa del Carmen, cuisine tacos, mole,
+          guacamole.
+        </li>
       </ul>
     </div>
   );
