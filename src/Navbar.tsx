c8645477146e09f410a40dac65d2fe89ff2928e9
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-17 01:08:15 +0100

    gitignore

---
+++ src/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="">
+        <h3 className="logo-text">Country Facts</h3>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
