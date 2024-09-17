037e39985cc663075f254c9842e8da7463117775
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-17 02:38:14 +0100

    dark toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [isDark, setIsDark] = useState(false);
+
   return (
-    <div className="App" data-theme="dark">
+    <div className="App" data-theme={isDark ? "dark" : "light"}>
       <h1 className="title">Hii Peeps</h1>
       <div className="box">
         <h2>This is a box</h2>
