3c9d7d54f3a4b0b2cac3be4c13375da935819c8e
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-17 01:11:30 +0100

    navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,14 +1,12 @@
 const Navbar = () => {
   return (
-    <div>
-      <nav className="">
-        <h3 className="logo-text">Country Facts</h3>
+    <nav className="">
+      <h3 className="logo-text">Country Facts</h3>
 
-        <div className="toggler--slider">
-          <div className="toggler--slider--circle"></div>
-        </div>
-      </nav>
-    </div>
+      <div className="toggler--slider">
+        <div className="toggler--slider--circle"></div>
+      </div>
+    </nav>
   );
 };
 
