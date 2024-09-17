29bfb8d8b8e60db10d9bbed110b3c42e61622ce1
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-17 01:31:51 +0100

    usestate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import Content from "./Content";
 import "./index.css";
+import Navbar from "./Navbar";
 
+const [darkMode, setDarkMode] useState\
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Content />
+    </div>
+  );
 };
 
 export default App;
